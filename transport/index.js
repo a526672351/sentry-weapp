02430b94ce5ae710eb3ef01a789f2f2ab4d1217c
huangqicong <huangqicong@wtoip.com>
2018-11-30 16:51:44 +0800

    feat: 优化为远程请求对象

--- transport/index.js
+++ transport/index.js
@@ -2,17 +2,18 @@
 import Raven from 'raven-js'
 
 Raven.setTransport(options => {
-  options.method = 'POST'
-  options.url = options.url + '?' + urlencode(options.auth)
-  wx.request(options).then(response => {
-    if (response.ok) {
-      options.onSuccess && options.onSuccess()
-    } else {
-      var error = new Error('Sentry error code: ' + response.status)
-      // It's called request only to keep compatibility with XHR interface
-      // and not add more redundant checks in setBackoffState method
-      error.request = response
-      options.onError && options.onError(error)
+  wx.request({
+    url: options.url + '?' + urlencode(options.auth),
+    method: 'POST',
+    header: {
+      'content-type': 'text/plain;charset=UTF-8'
+    },
+    data: options.data,
+    success: function() {
+      options.onSuccess && options.onSuccess();
+    },
+    fail: function(err) {
+      options.onError && options.onError(err);
     }
   })
 })
